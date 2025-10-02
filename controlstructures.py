c6fe331b2ebaff0e70f34f0d3b47003f76c9fdca
TatendaZowa <zvikotate1710@gmail.com>
2025-10-02 09:01:49 +0200

    controlstructures.py

--- controlstructures.py
+++ controlstructures.py
@@ -1,21 +1,24 @@
 #Selection statements
 
 #If statemeents 
-#If-else statements 
-'''
-VOTING ELIGIBILITY
-'''
-age = 17
+#1.If-else statements 
+#Provides an alternative code block when the if condition is False
+print('''
+1. VOTING ELIGIBILITY(If-else statements)
+      ''')
+age = 0
 if age >=18:
     print("You are eligible to vote")
 else:
     print("You are not eligible to vote")
 
-#If-elif-else statements 
+
+#2.If-elif-else statements 
+#For handling multiple, mutually exclusive conditions in a cascading manner
 print('''
-      GRADES
+2. GRADES(If-elif-else statements)
       ''')
-grade = 33
+grade = 0
 if grade >= 90: 
     print("Gade: A+")
 elif grade >= 80:
@@ -29,15 +32,19 @@ elif grade >= 50:
 else:
     print("You failed, Grade: F")
 
-#Nested loops 
 
+#3.Nested loops 
+#If statements can be nested within other if, elif or else blocks to handle more complex conditional logic
 print('''
-GEAR SHIFT COMMANDS
+3. GEAR SHIFT COMMANDS(Nested Loop)
       ''')
-speed = 66
-gearshift = True
-if speed >= 0 and speed <= 10: 
-    print("stay in the first gear")
+speed = 0
+gearshift = False
+if speed == 0:
+    if gearshift:
+        print("shift into the first gear")
+    else:
+        print("you are already in the first gear")
 elif speed >=10 and speed <=25:
     if gearshift: 
         print("shift into the second gear")
@@ -57,4 +64,56 @@ elif speed > 80 :
     if gearshift: 
         print("shift into the fifth gear")
     else:
-        print("you are already in the fifth gear")        
+        print("you are already in the fifth gear")   
+
+
+#Looping statements     
+#1.For loop
+#Execute code for each sequence
+print('''
+4. CAR LIST(For loop)
+      ''')
+#Iterating through a sequence
+cars = ["BMW", "Benz", "Buggati", "Brabus"]
+for car in cars: #Iterates all elements in the list
+    print(f"I like {car}")
+
+#Using Enumerate
+#Provides both the index and the value during iteration. Useful for tasks requiring element position
+print('''
+5. CAR LIST(Enumerate Function)
+      ''')
+for index, value in enumerate(cars): 
+    print(f"{index}: {value}")
+
+#Using Range - Generates a sequence of numbers
+print('''
+6. NUMBERS(Range Function)
+      ''')
+#Range(Stop)- It starts from 0 but does not include the stop value
+#Range(start, stop)- Starts from the start value up to the value before the stop value
+#Range(start, stop, step)- Includes a step value
+for i in range(0, 10, 3):
+    print(i)
+
+
+#2. While loops
+#Execute code as long as a specified condition remains True 
+#Condition-controlled loop- the loop continues until the condition becomes False 
+print('''
+7. COUNT(While loop)
+      ''')
+count = 0 
+while count < 10: #Loops continues as long as count is less than 10
+    print(f"count is: {count}")
+    count += 1 #Crucial update to the condition variable to avoid infinite loop
+
+#Infinite loops- occur if the condition never becomes false. 
+#Must be handled carefully, often with break statements based on other conditions
+print('''
+8. USER_INPUT(Infinite loop)
+      ''')
+while True:
+    user_input = int(input("Enter integer between 1 to 10: "))
+    if user_input == 5:
+        break# Exit the loop if condition is met 
